7694995c0faf352c19e55d8075e7a5dae1003de0
igor <mizikikou@gmail.com>
2026-08-06 16:28:32 +0800

    Django 0806 作業

--- homework2/myapp/views.py
+++ homework2/myapp/views.py
@@ -1,27 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-
-def sayhello(request):
-    return HttpResponse("<b>Hello World 40!!!!!</b>")
-
-def hello1(request,username):
+def homework2(request, username):
     print(username)
-    return HttpResponse(f"<b>Hello {username}!!!!!</b>")
-
-from datetime import datetime  
-def hello2(request,username):
-    print(username)
-    now = datetime.now()  # get current date and time
-    current_time = now.strftime("%H:%M:%S")  # format time as HH
-    print("Current Time =", current_time)
-    # return HttpResponse(f"<b>Hello {username}!!!!!</b>")
-    return render(request, 'hello2.html', locals())
-  
-def hello3(request,username):
-    print(username)
-    now = datetime.now()  # get current date and time
-    current_time = now.strftime("%H:%M:%S")  # format time as HH
-    print("Current Time =", current_time)
-    # return HttpResponse(f"<b>Hello {username}!!!!!</b>")
-    return render(request, 'hello3.html', locals())
+    # locals() 會將這裡的 username 變數自動打包送給 HTML 網頁
+    return render(request, 'homework2.html', locals())
