acb16e37bd9fa98528437029f8931c2b68032f4a
igor <mizikikou@gmail.com>
2026-08-07 09:36:04 +0800

    修正 0806 homework2作業

--- homework2/homework2/urls.py
+++ homework2/homework2/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for project1 project.
+URL configuration for homework2 project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/6.0/topics/http/urls/
--- homework2/myapp/views.py
+++ homework2/myapp/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 def homework2(request, username):
     print(username)
     # locals() 會將這裡的 username 變數自動打包送給 HTML 網頁
-    return render(request, 'homework2.html', locals())
+    return render(request, 'show.html', locals())
