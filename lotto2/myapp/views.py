d4f8e7291e982ec18b5528e8274c8f08845f217c
igor <mizikikou@gmail.com>
2026-08-10 09:46:38 +0800

    0810 更新lotto2 ???

--- lotto2/myapp/views.py
+++ lotto2/myapp/views.py
@@ -8,9 +8,9 @@ from django.shortcuts import render
 def lotto2(request):
     
     # 產生 6 組大樂透號碼
-    lotto_groups = []
+    lotto_groups = []   # 在使用「列表（List）」的 append() 方法之前，必須先告訴程式「這是一個空的列表」。
     for _ in range(6):  #不需要在迴圈內部用到計數器的值時，用_來代替變數名稱。
-        numbers = sorted(random.sample(range(1, 50), 6))
+        numbers = sorted(random.sample(range(1, 43), 6))
         # 將每個個位數轉成補0的字串
         #formatted_numbers = [f"{num:02d}" for num in numbers]
         #lotto_groups.append(formatted_numbers)
