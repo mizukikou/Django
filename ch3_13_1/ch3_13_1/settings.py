a7b54cc8c0b7da85d71470eddc9440504d99ff99
igor <mizikikou@gmail.com>
2026-08-13 15:34:51 +0800

    0813 資料庫教學

--- ch3_13_1/ch3_13_1/settings.py
+++ ch3_13_1/ch3_13_1/settings.py
@@ -75,8 +75,8 @@ WSGI_APPLICATION = 'ch3_13_1.wsgi.application'
 
 DATABASES = {
     'default': {
-        #'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
         
         #mysql
          'ENGINE': 'django.db.backends.mysql',
