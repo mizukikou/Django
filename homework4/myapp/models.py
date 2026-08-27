2a8954b7d88822bc3674e576af7bb6e4792fc107
igor <mizikikou@gmail.com>
2026-08-27 08:34:22 +0800

    0827 完成 homewrok4

--- homework4/myapp/models.py
+++ homework4/myapp/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class TemperatureRecord(models.Model):
-    id = models.AutoField(primary_key=True)
+    myid = models.AutoField(primary_key=True)  # 將 id 改為 myid
     sensor_id = models.IntegerField()
     temperature = models.FloatField()
     humidity = models.FloatField()
--- homework4/myapp/views.py
+++ homework4/myapp/views.py
@@ -1,18 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from myapp.models import *
-from django.forms.models import model_to_dict
+from myapp.models import TemperatureRecord  # 1. 修改為您的 Model 名稱
 
-# Create your views here.
 def homework4(request):
     return HttpResponse("Hello, this is homework4!")
 
 def view_history_temperature(request):
-    # 取得所有的溫度紀錄
-    temperature_records = TemperatureRecord.objects.all()
-
-    # 將每個紀錄轉換為字典
-    records_list = [model_to_dict(record) for record in temperature_records]
-
-    # 將紀錄傳遞給模板
-    return render(request, 'view_history_temperature.html', {'records': records_list})
+    # 2. 這裡的主鍵是 id，所以要改成依 '-id' 降冪排序
+    temperature_records = TemperatureRecord.objects.all().order_by('-myid')
+    return render(request, 'view_history_temperature.html', {'records': temperature_records})
